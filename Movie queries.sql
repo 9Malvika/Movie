e99b373d3dc41b7df2705c1adace0cb003e1d25d
9Malvika <92156749+9Malvika@users.noreply.github.com>
2021-10-14 16:07:52 +0530

    Update Movie queries.sql

--- Movie queries.sql
+++ Movie queries.sql
@@ -1,1 +1,21 @@
+--Query 1
+
+--List the name of all movies directed by 'Rajkumar Hirani' and for this we use the DIRECTOR table.
+-- SELECT DIR_ID
+-- FROM DIRECTOR
+-- WHERE DIR_ID = 'Rajkumar Hirani'
+-- the above query will return the DIR_ID "
+
+SELECT MOV_NAME
+FROM MOVIE
+WHERE DIR_ID = (SELECT DIR_ID
+FROM DIRECTOR
+WHERE DIR_NAME='Rajkumar Hirani');
+
+-- the above query will return the DIR_ID "
+---------------------------------------------------------------------
+
+--Query 2
+
+
 
