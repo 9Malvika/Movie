5f5b0bab68e49b3287cc971a16dc827a18759d6f
9Malvika <92156749+9Malvika@users.noreply.github.com>
2021-10-14 16:15:17 +0530

    Update Movie queries.sql

--- Movie queries.sql
+++ Movie queries.sql
@@ -4,7 +4,7 @@
 -- SELECT DIR_ID
 -- FROM DIRECTOR
 -- WHERE DIR_ID = 'Rajkumar Hirani'
--- the above query will return the DIR_ID "
+-- the above query will return the DIR_ID "201". Now we will find the name of the movie whose DIR_ID is "201". For using this we use table MOVIE and use the above query as a sub query.
 
 SELECT MOV_NAME
 FROM MOVIE
@@ -12,10 +12,18 @@ WHERE DIR_ID = (SELECT DIR_ID
 FROM DIRECTOR
 WHERE DIR_NAME='Rajkumar Hirani');
 
--- the above query will return the DIR_ID "
 ---------------------------------------------------------------------
 
 --Query 2
 
+-- display all the actress who acted in a movie between 2009 and 2012
+-- we use ACTRESS_NAME field of ARTIST table as well as the MOV_YEAR field of the MOVIE table.
+-- for joining these two tables we use a third table CAST using ART_ID and MOV_ID.
+
+SELECT A.ACTRESS_NAME, M.MOV_NAME, M.MOV_YEAR
+FROM ARTIST A, CAST C, MOVIE M
+WHERE A.ART_ID = C.ART_ID
+AND C.MOV_ID = M.MOV_ID
+AND M.MOV_YEAR BETWEEN 2009 AND 2012
 
 
