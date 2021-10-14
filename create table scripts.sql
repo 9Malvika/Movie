722787b69549966b8ce82761671ab7c4d41d0c82
9Malvika <92156749+9Malvika@users.noreply.github.com>
2021-10-14 15:36:47 +0530

    Create create table scripts.sql

---
+++ create table scripts.sql
@@ -0,0 +1,33 @@
+--Create Table ARTIST with Primary Key as ART_ID
+
+CREATE TABLE ARTIST (
+ART_ID INTEGER PRIMARY KEY,
+ACTOR_NAME VARCHAR(20),
+ACTRESS_NAME VARCHAR(20));
+
+DESC ARTIST;
+
+----------------------------
+
+--Create Table DIRECTOR with Primary Key as DIR_ID
+
+CREATE TABLE DIRECTOR(
+DIR_ID INTEGER PRIMARY KEY,
+DIR_NAME VARCHAR(20));
+
+DESC DIRECTOR;
+
+----------------------------
+
+--Create Table MOVIE with Primary Key as MOV_ID and Foreign Key DIR_ID referring DIRECTOR table
+
+CREATE TABLE MOVIE(
+MOV_ID INTEGER PRIMARY KEY,
+MOV_NAME VARCHAR(25),
+MOV_YEAR INTEGER,
+DIR_ID INTEGER,
+FOREIGN KEY (DIR_ID) REFERENCES DIRECTOR(DIR_ID));
+
+DESC MOVIE;
+
+----------------------------
